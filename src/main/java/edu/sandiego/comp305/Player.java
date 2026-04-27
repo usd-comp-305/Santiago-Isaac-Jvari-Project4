bf4876ed246b30cd8f88c91231628d6a1bbad6d4
Isaac <fajardoisaac21@gmail.com>
2026-04-27 10:06:20 -0700

    Feat: Player Stubs Completed

---
+++ src/main/java/edu/sandiego/comp305/Player.java
@@ -0,0 +1,38 @@
+
+public class Player {
+    String name;
+    int health;
+    int gold;
+    Weapon weapon;
+    Armor armor;
+    ArrayList<items> inventory;
+    AttackStrategy attackStrategy;
+
+    public void attack(){
+
+    }
+    public void takeDamage(int damage){
+    }
+    public void heal (int health){
+
+    }
+    public void buyItem(Item item){
+
+    }
+    public void usePotion(Potion potion){
+    }
+    public void equipWeapon(Weapon weapon){
+
+    }
+    public void equipArmor(Armor armor){
+
+    }
+    public void setAttackStrategy(AttackStrategy strategy){
+
+    }
+    public boolean isAlive(){
+        return false;
+    }
+}
+
+
--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -1,5 +1,3 @@
-package edu.sandiego.comp305;
-
 /**
  * Utility class for String operations.
  */
