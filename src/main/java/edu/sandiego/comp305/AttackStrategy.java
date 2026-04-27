762866a85d19eacd34769cd4c779601b3f48c079
J’Vari Miller <jvarimiller@sandiego.edu>
2026-04-27 12:01:33 -0700

    Added attack strategy with failing tests with some basic attacks

---
+++ src/main/java/edu/sandiego/comp305/AttackStrategy.java
@@ -0,0 +1,5 @@
+package edu.sandiego.comp305;
+
+public interface AttackStrategy {
+    int executeAttack(int attackerPower);
+}
---
+++ src/test/java/edu/sandiego/comp305/AttackStrategyTests.java
@@ -0,0 +1,24 @@
+import edu.sandiego.comp305.AttackStrategy;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttackStrategyTests {
+
+    @Test
+    public void testBasicAttackReturnsSamePower() {
+        AttackStrategy strategy = new BasicAttack();
+
+        int damage = strategy.executeAttack(10);
+
+        assertEquals(10, damage);
+    }
+
+    @Test
+    public void testHeavyAttackDoesMoreDamage() {
+        AttackStrategy strategy = new HeavyAttack();
+
+        int damage = strategy.executeAttack(10);
+
+        assertTrue(damage > 10);
+    }
+}
