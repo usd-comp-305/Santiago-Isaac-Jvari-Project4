b73302616dca12625c5b4f3ed32f9006a4a01db1
J’Vari Miller <jvarimiller@sandiego.edu>
2026-04-27 23:46:23 -0700

    added EnemyFactory stubs with tests that should fail

---
+++ src/main/java/edu/sandiego/comp305/EnemyFactory.java
@@ -0,0 +1,16 @@
+package edu.sandiego.comp305;
+
+public class EnemyFactory {
+    public class EnemyFactory {
+
+        public Enemy createEnemy(String type) {
+            // TODO
+            return null;
+        }
+
+        public BossEnemy createBoss() {
+            // TODO
+            return null;
+        }
+    }
+}
