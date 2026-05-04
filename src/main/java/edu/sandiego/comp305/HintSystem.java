1bb8696d0adfc0fd9f63f38515107a8d5aaa3b1e
J’Vari Miller <jvarimiller@sandiego.edu>
2026-05-03 22:36:16 -0700

    Checkstyle fixes

--- src/main/java/edu/sandiego/comp305/HintSystem.java
+++ src/main/java/edu/sandiego/comp305/HintSystem.java
@@ -5,13 +5,17 @@ public class HintSystem {
     public String getHintForEnemy(final Enemy enemy,
             final Direction direction) {
 
-        return getDirectionalMessage(direction, enemy.getHint());
+        return getDirectionalMessage(
+                direction, enemy.getHint());
     }
 
-    public String getDirectionalMessage(final Direction direction,
+    public String getDirectionalMessage(
+            final Direction direction,
             final String hint) {
 
-        return "To the " + direction.toString().toLowerCase() + " you hear " + hint;
+        return "To the " +
+                direction.toString().toLowerCase() +
+                " you hear " + hint;
     }
 }
 
