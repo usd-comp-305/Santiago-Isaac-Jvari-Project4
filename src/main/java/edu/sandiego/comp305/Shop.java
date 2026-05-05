6fce6fe6c026b94af025339b74f6a9fa0776acf6
Isaac <fajardoisaac21@gmail.com>
2026-05-05 15:10:06 -0700

    Feat: Implmented displayItems, buyweaponUpgrade and wrote tests for them

--- src/main/java/edu/sandiego/comp305/Shop.java
+++ src/main/java/edu/sandiego/comp305/Shop.java
@@ -3,17 +3,44 @@ package edu.sandiego.comp305;
 import java.util.ArrayList;
 
 public class Shop {
-    public void displayItems(){
+    private ArrayList<Item> avaliableItems = new ArrayList<>();
 
-    }
-    public void buyWeaponUpgrade(int boostAmount,int price, Weapon weapon, Player player){
-        player.buyItem(weapon,boostAmount);
-    }
-    public void buyArmorUpgrade(int price, Armor armor){
-
+    public ArrayList<Item> displayItems(){
+        return new ArrayList<>(avaliableItems);
     }
 
-    public void buyPotion(int price, Potion potion){
+    public void addItem(final Item item){
+        avaliableItems.add(item);
+    }
+
+    public boolean buyWeaponUpgrade(
+            final int boostAmount,
+            final int price,
+            final Player player){
+        if (canPlayerAfford(player,price)){
+            if (player.weapon != null){
+                player.weapon.attackBoost += boostAmount;
+                player.decreaseGold(price);
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private boolean canPlayerAfford(final Player player, final int price){
+        if (player.gold >= price) {
+            return true;
+        }
+        return false;
+    }
+
+    public void buyArmorUpgrade(final int price, final Armor armor){
+
+    }
+
+    public void buyPotion(final int price, final Potion potion){
 
     }
 
--- src/test/java/edu/sandiego/comp305/ShopTest.java
+++ src/test/java/edu/sandiego/comp305/ShopTest.java
@@ -8,17 +8,50 @@ class ShopTest {
 
     @Test
     void displayItems() {
-        Shop shop = new Shop();
-        shop.displayItems();
     }
 
     @Test
     void buyWeaponUpgrade() {
-        Shop shop = new Shop();
-        Player player = new Player();
-        var sword = ItemFactory.createSwordWeapon(0);
-        player.buyItem(sword,10);
+        final Shop shop = new Shop();
+        final Player player = new Player("Isaac",10,10);
+
+        final Weapon sword = ItemFactory.createSwordWeapon(1);
+        player.equipWeapon(sword);
+
+        final boolean result = shop.buyWeaponUpgrade(10, 10, player);
+
+        assertTrue(result);
+        assertEquals(0, player.getGold());
+        assertEquals(20, player.weapon.attackBoost);
     }
+
+    @Test
+    void buyWeaponUpGradeNoWeapon(){
+        final Shop shop = new Shop();
+        final Player player = new Player("Isaac",10,10);
+        assertTrue(player.gold == 10);
+        assertFalse(shop.buyWeaponUpgrade(10,10,player));
+        assertTrue(player.gold == 10);
+    }
+
+    @Test
+    void buyArmorUpGrade(){
+        final Shop shop = new Shop();
+//        final Player player = new Player();
+        final Armor armor = ItemFactory.createArmor(1);
+        shop.buyArmorUpgrade(10,armor);
+    }
+
+    @Test
+    void buyPotionHealth(){
+        final Shop shop = new Shop();
+//        final Player player = new Player();
+
+        final Potion health = ItemFactory.createPotion(PotionType.HEALTH);
+
+        shop.buyPotion(10,health);
+    }
+
 
     @Test
     void buyArmorUpgrade() {
@@ -27,4 +60,6 @@ class ShopTest {
     @Test
     void buyPotion() {
     }
+
+
 }
