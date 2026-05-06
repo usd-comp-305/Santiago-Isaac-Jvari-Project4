ac28b712c6747e0fa5aeae5d7c331d4c58d65c06
Isaac <fajardoisaac21@gmail.com>
2026-05-06 10:48:10 -0700

    Feat: Added a tier field to Weapon, added setter and getter methods for it and made tier private

--- src/main/java/edu/sandiego/comp305/Weapon.java
+++ src/main/java/edu/sandiego/comp305/Weapon.java
@@ -2,6 +2,23 @@ package edu.sandiego.comp305;
 
 public class Weapon extends Item{
     public int attackBoost;
-    public int tier;
+
+    private int tier;
+
+    public int getTier(){
+        return tier;
+    }
+
+    public void setTier(final int tier){
+        this.tier = tier;
+    }
+
+    public void setAttackBoost(final int attackBoost){
+        this.attackBoost = attackBoost;
+    }
+
+    public int getAttackBoost(){
+        return attackBoost;
+    }
 }
 
