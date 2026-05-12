30216410f990a2630f6e887e105600450c378039
Isaac <fajardoisaac21@gmail.com>
2026-05-11 22:49:21 -0700

    fix: cleaned up code to follow SOLID principles, Item factory follows OCP

--- src/main/java/edu/sandiego/comp305/ItemFactory.java
+++ src/main/java/edu/sandiego/comp305/ItemFactory.java
@@ -8,65 +8,29 @@ public final class ItemFactory {
     }
 
     public static Weapon createSwordWeapon(final int tier){
+        final SwordTier base = SwordTier.fromInt(tier);
         final Weapon sword = new Weapon();
-        if (tier == 1){
-            sword.name = "Wood Sword";
-            sword.cost = 10;
-            sword.setAttackBoost(10);
-            sword.setTier(1);
-        } else if (tier == 2){
-            sword.name = "Steel Sword";
-            sword.cost = 20;
-            sword.setAttackBoost(20);
-            sword.setTier(2);
-        } else if (tier == 3) {
-            sword.name = "Diamond Sword";
-            sword.cost = 30;
-            sword.setAttackBoost(30);
-            sword.setTier(3);
-        }
+        sword.setName(base.getName());
+        sword.setCost(base.getCost());
+        sword.setAttackBoost(base.getAttackBoost());
+        sword.setTier(base.getTier());
         return sword;
     }
 
     public static Armor createArmor(final int tier){
+        final ArmorTier baseArmor = ArmorTier.fromInt(tier);
         final Armor armor = new Armor();
-        if (tier == 1){
-            armor.name = "Wood Armor";
-            armor.cost = 10;
-            armor.setDefenseBoost(armor.getDefenseBoost() + 10);
-        } else if (tier == 2){
-            armor.name = "Steel Armor";
-            armor.cost = 20;
-            armor.setDefenseBoost(armor.getDefenseBoost() + 20);
-        }  else if (tier == 3) {
-            armor.name = "Diamond Armor";
-            armor.cost = 30;
-            armor.setDefenseBoost(armor.getDefenseBoost() + 30);
-        }
+        armor.setName(baseArmor.getName());
+        armor.setCost(baseArmor.getCost());
+        armor.setDefenseBoost(baseArmor.getDefenseBoost());
         return armor;
     }
 
     public static Potion createPotion(final PotionType type){
-        switch (type){
-            case HEALTH -> {
-                final Potion potion = new HealthPotion();
-                potion.name = "Healing Potion";
-                potion.cost = 10;
-                potion.setEffectValue(30);
-                return potion;
-
-
-            }
-            case STRENGTH -> {
-                final Potion potion = new StrengthPotion();
-                potion.name = "Strength Potion";
-                potion.cost = 10;
-                potion.setEffectValue(10);
-                return potion;
-            }
-            default -> {
-                return null;
-            }
-        }
+        final Potion potion = type.createPotion();
+        potion.setName(type.getName());
+        potion.setCost(type.getCost());
+        potion.setEffectValue(type.getEffectValue());
+        return potion;
     }
 }
--- src/main/java/edu/sandiego/comp305/StrengthPotion.java
+++ src/main/java/edu/sandiego/comp305/StrengthPotion.java
@@ -3,9 +3,6 @@ package edu.sandiego.comp305;
 public class StrengthPotion extends Potion{
     @Override
     public void applyEffect(final Player player ) {
-
-        player.getWeapon().setAttackBoost(
-                player.getWeapon().getAttackBoost() + getEffectValue()
-        );
+        player.boostWeapon(getEffectValue());
     }
 }
